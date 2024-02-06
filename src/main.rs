84a82f56b052482ca8f3174da38abaac9476400c
Johan Chataigner <jchataigner@betomorrow.com>
2024-02-06 15:06:27 +0100

    Add basic version handling both case sensitive and insensitive search

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use std::{env, process};
+use zero2prod::Config;
+
 fn main() {
-    let toto = 2;
-    println!("Hello {toto}, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|error| {
+        eprintln!("Problem parsing arguments: {error}");
+        process::exit(1)
+    });
+
+    if let Err(e) = zero2prod::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
